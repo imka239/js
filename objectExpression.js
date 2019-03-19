224606ad63e62dcc1e7803d0e35e8a5cb8123f11
imka239 <ima2ima3go9@gmail.com>
2019-03-19 16:35:24 +0300

    Add files via upload

--- objectExpression.js
+++ objectExpression.js
@@ -11,154 +11,141 @@ const vars = {
     "w" : 5
 };
 
-Operation = {toString : function () {
-        let s = ""
-        for (i of arguments) {
-            s += i;
-        }
-        return s;
-    }, evaluate : function(...args) {
-    
-    }
-};
-
 const Const = function(x) {
     this.evaluate = () => x;
-    this.toString = () => Operation.toString(x);
+    this.toString = () => x + "";
     this.diff = () => new Const(0);
     this.simplify = () => this
 };
 
 const Variable = function(x) {
-    this.evaluate = (...args) => args[vars[x]]
+    this.evaluate = (...args) => args[vars[x]];
     this.diff = (val) => new Const(val === x ? 1 : 0);
-    this.toString = () => Operation.toString(x);
+    this.toString = () => x + "";
     this.simplify = () => this
-}
+};
+
+const Op = {
+    evaluate: (operation) => (...args) => (...vars) => {
+        return operation(...args.map(arg => arg.evaluate(...vars)));
+    }
+    , toString : (...args) => {
+        let s = "";
+        for (i of args) {
+            s += i + " ";
+        }
+        return s;
+    }
+    , diff : (arg) => (val) => arg.diff(val)
+    , simplify : (operation, Op) => (...args) => {
+        args = args.map(arg => arg.simplify());
+        let onlyConst = 1;
+        args.forEach(token => {if (!isConst(token)) {onlyConst = 0;}});
+        if (onlyConst) {
+            return new Const(operation(...args.map(arg => arg.evaluate())));
+        } else {
+            return new Op(...args);
+        }
+    }
+};
+
 
 function Negate(x) {
-    this.evaluate = (...args) => -x.evaluate(...args);
-    this.diff = (val) => new Negate(x.diff(val));
-    this.toString = () => Operation.toString(x, " negate");
-    this.simplify = () => {
-        let new_x = x.simplify();
-        if (isConst(new_x)) {
-            return new Const(-new_x);
-        }
-        return new Negate(new_x);
-    }
+    this.evaluate = Op.evaluate(a => -a)(x);
+    this.diff = (val) => new Negate(Op.diff(x)(val));
+    this.toString = () => Op.toString(x) + "negate";
+    this.simplify = () => Op.simplify(a => -a, Negate)(x);
 }
 
 function ArcTan(x) {
-    this.evaluate = (...args) => Math.atan(x.evaluate(...args));
-    this.diff = (val) => new Divide(x.diff(val), new Add(new Const(1), new Multiply(x, x)));
-    this.toString = () => Operation.toString(x, " atan");
-    this.simplify = () => {
-        let new_x = x.simplify();
-        if (isConst(new_x)) {
-            return new Const(Math.atan(new_x));
-        }
-        return new ArcTan(new_x);
-    }
+    this.evaluate = Op.evaluate(a => Math.atan(a))(x);
+    this.diff = (val) => new Divide(Op.diff(x)(val), new Add(new Const(1), new Multiply(x, x)));
+    this.toString = () => Op.toString(x) + "atan";
+    this.simplify = () => Op.simplify(a => Math.atan(a), ArcTan)(x);
 }
 
 function Add(x, y) {
-    this.evaluate = (...args) => x.evaluate(...args) + y.evaluate(...args);
-    this.diff = (val) => new Add(x.diff(val), y.diff(val))
-    this.toString = () => Operation.toString(x, " ", y, " +")
+    this.evaluate = Op.evaluate((a, b) => a + b)(x, y);
+    this.diff = (val) => new Add(Op.diff(x)(val), Op.diff(y)(val));
+    this.toString = () => Op.toString(x, y) + "+";
     this.simplify = () => {
-        let new_x = x.simplify();
-        let new_y = y.simplify();
-        if (isZero(new_x)) {
-            return new_y;
+        let simple = Op.simplify((a, b) => a + b, Add)(x, y);
+        let simpleX = x.simplify();
+        let simpleY = y.simplify();
+        if (isZero(simpleX)) {
+            return simpleY;
         }
-        if (isZero(new_y)) {
-            return new_x;
+        if (isZero(simpleY)) {
+            return simpleX;
         }
-        if (isConst(new_x) && isConst(new_y)) {
-            return new Const(new_x + new_y);
-        }
-        return new Add(new_x, new_y);
+        return simple;
     }
 }
 
 function ArcTan2(x, y) {
-    this.evaluate = (...args) => Math.atan2(x.evaluate(...args), y.evaluate(...args));
-    this.diff = (val) => new Divide(new Subtract(new Multiply(x.diff(val), y), new Multiply(x, y.diff(val))),
-        new Add(new Multiply(x, x), new Multiply(y, y)))
-    this.toString = () => Operation.toString(x, " ", y, " atan2")
-    this.simplify = () => {
-        let new_x = x.simplify();
-        let new_y = y.simplify();
-        if (isConst(new_x) && isConst(new_y)) {
-            return Math.atan2(new_x, new_y);
-        }
-        return new ArcTan2(new_x, new_y);
-    }
+    this.evaluate = Op.evaluate((a, b) => Math.atan2(a, b))(x, y);
+    this.diff = (val) => new Divide(new Subtract(new Multiply(Op.diff(x)(val), y), new Multiply(x, Op.diff(y)(val))),
+        new Add(new Multiply(x, x), new Multiply(y, y)));
+    this.toString = () => Op.toString(x, y) + "atan2";
+    this.simplify = () => Op.simplify((a, b) => Math.atan2(a, b), ArcTan2)(x, y);
 }
 
 function Subtract(x, y) {
-    this.evaluate = (...args) => x.evaluate(...args) - y.evaluate(...args);
-    this.diff = (val) => new Subtract(x.diff(val), y.diff(val))
-    this.toString = () => Operation.toString(x, " ", y, " -")
-    this.simplify = function() {
-        let new_x = x.simplify();
-        let new_y = y.simplify();
-        if (isZero(new_x)) {
-            return new Negate(new_y).simplify();
+    this.evaluate = Op.evaluate((a, b) => a - b)(x, y);
+    this.diff = (val) => new Subtract(x.diff(val), y.diff(val));
+    this.toString = () => Op.toString(x, y) + "-";
+    this.simplify = () => {
+        let simple = Op.simplify((a, b) => a - b, Subtract)(x, y);
+        let simpleX = x.simplify();
+        let simpleY = y.simplify();
+        if (isZero(simpleX)) {
+            return new Negate(simpleY).simplify();
         }
-        if (isZero(new_y)) {
-            return new_x;
+        if (isZero(simpleY)) {
+            return simpleX;
         }
-        if (isConst(new_x) && isConst(new_y)) {
-            return new Const(new_x - new_y)
-        }
-        return new Subtract(new_x, new_y);
+        return simple;
     }
 }
 
 function Multiply(x, y) {
-    this.evaluate = (...args) => x.evaluate(...args) * y.evaluate(...args);
-    this.toString = () => Operation.toString(x, " ", y, " *")
-    this.diff = (val) => new Add(new Multiply(x.diff(val), y), new Multiply(y.diff(val), x));
-    this.simplify = function () {
-        let new_x = x.simplify();
-        let new_y = y.simplify();
-        if (isZero(new_x) || isZero(new_y)) {
+    this.evaluate = Op.evaluate((a, b) => a * b)(x, y);
+    this.toString = () => Op.toString(x, y) + "*";
+    this.diff = (val) => new Add(new Multiply(x.diff(val), y), new Multiply(x, y.diff(val)));
+    this.simplify = () => {
+        let simple = Op.simplify((a, b) => a * b, Multiply)(x, y);
+        let simpleX = x.simplify();
+        let simpleY = y.simplify();
+        if (isZero(simpleX) || isZero(simpleY)) {
             return new Const(0);
         }
-        if (isOne(new_y)) {
-            return new_x;
+        if (isOne(simpleY)) {
+            return simpleX;
         }
-        if (isOne(new_x)) {
-            return new_y;
+        if (isOne(simpleX)) {
+            return simpleY;
         }
-        if (isConst(new_x) && isConst(new_y)) {
-            return new Const(new_x * new_y);
-        }
-        return new Multiply(new_x, new_y);
+        return simple;
     }
 }
 
+
 function Divide(x, y) {
-    this.evaluate = (...args) => x.evaluate(...args) / y.evaluate(...args);
-    this.toString = () => Operation.toString(x, " ", y, " /")
+    this.evaluate = Op.evaluate((a, b) => a / b)(x, y);
+    this.toString = () => Op.toString(x, y) + "/";
     this.diff = (val) => new Divide(new Subtract(
             new Multiply(x.diff(val), y), new Multiply(x, y.diff(val))), new Multiply(y, y));
-    this.simplify = function () {
-        console.log(x)
-        let new_x = x.simplify();
-        let new_y = y.simplify();
-        if (isZero(new_x)) {
+    this.simplify = () => {
+        let simple = Op.simplify((a, b) => a / b, Divide)(x, y);
+        let simpleX = x.simplify();
+        let simpleY = y.simplify();
+        if (isZero(simpleX)) {
             return new Const(0);
         }
-        if (isOne(new_y)) {
-            return new_x;
+        if (isOne(simpleY)) {
+            return simpleX;
         }
-        if (isConst(new_x) && isConst(new_y)) {
-            return new Const(new_x / new_y);
-        }
-        return new Divide(new_x, new_y);
+        return simple;
     }
 }
 
@@ -170,11 +157,6 @@ const oper = {
     "negate" : Negate,
     "atan" : ArcTan,
     "atan2" : ArcTan2
-    //"min3" : Min3,
-    //"max5" : Max5,
-    //"avg5" : avg5,
-    //"med3" : med3
-
 };
 
 const need = {
@@ -185,33 +167,23 @@ const need = {
     "negate" : 1,
     "atan" : 1,
     "atan2" : 2
-    //"min3" : 3,
-    //"max5" : 5,
-    //"avg5" : 5,
-    //"med3" : 3
-
 };
 
 function parse(source) {
-    const tokens = source.split(' ').filter(s => s.length > 0);
     let stack = [];
-    for (let i of tokens) {
-        if (i in vars) {
-            stack.push(new Variable(i))
-        } else if (i in oper) {
-            let args = [];
-            for (let j = 0; j < need[i]; j++) {
-                args.push(stack.pop())
-            }
-            args.reverse();
-            stack.push(new oper[i](...args))
+    const tokens = source.split(' ').filter(s => s.length > 0).forEach(token => {
+        if (token in vars) {
+            stack.push(new Variable(token))
+        } else if (token in oper) {
+            let args = stack.splice(stack.length - need[token], need[token]);
+            stack.push(new oper[token](...args))
         } else {
-            stack.push(new Const(parseInt(i)))
+            stack.push(new Const(parseInt(token)))
         }
-    }
+    });
     return stack.pop()
 }
 
 
-let A = new ArcTan2(new Add(new Variable('x'), new Variable('y')), new Variable('z')).diff('x')
-console.log(A)
+let A = parse('x y z * /').diff('y').simplify().evaluate(1, 2, 3);
+console.log(A);
