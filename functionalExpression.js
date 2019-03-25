ed35e08a759737233494078b9d3f583283c59c9b
imka239 <ima2ima3go9@gmail.com>
2019-03-25 16:13:33 +0300

    Add files via upload

--- functionalExpression.js
+++ functionalExpression.js
@@ -9,27 +9,25 @@ const vars = {
 
 const operation = f => (...args) => (...eval) => {
     let result = args.map(arg => arg(...eval));
-    //let res = []
-    //for (const arg of args) {
-    //    res.push(arg(...eval))
-    //}
     return f(...result)
 };
 
 const cnst = a => () => a;
-function variable(name) {
-    return (...args) => args[vars[name]];
-}
+let variable = (name) => {
+    const id = vars[name];
+    return (...args) => args[id]
+};
+
 
 const subtract = operation((a, b) => (a - b));
 const multiply = operation((a, b) => (a * b));
 const add = operation((a, b) => a + b);
 const divide = operation((a, b) => a / b);
 const negate = operation((a) => -a);
-const min3 = operation(() => Math.min.apply(null, arguments));
-const max5 = operation(() => Math.max.apply(null, arguments));
-const avg5 = operation((...args) => args.reduce((a, b) => a + b) / 5);
-const med3 = operation((...args) => args.sort((x, y) => (x - y))[1]);
+const min3 = operation(Math.min);
+const max5 = operation(Math.max);
+const avg5 = operation((...args) => args.reduce((a, b) => a + b) / args.length);
+const med3 = operation((...args) => args.sort((x, y) => (x - y))[Math.floor(args.length / 2)]);
 const e = cnst(Math.E);
 const pi = cnst(Math.PI);
 
@@ -61,26 +59,29 @@ const need = {
     "max5" : 5,
     "avg5" : 5,
     "med3" : 3
-
 };
+
+const VARIABLE = {};
+for (v in vars) {
+    VARIABLE[v] = variable(v);
+}
 
 const parse = source => {
     let stack = [];
-    const tokens = source.split(' ').filter(s => s.length > 0);
-    for (let i of tokens) {
-        if (i in consts) {
-            stack.push(consts[i])
-        } else if (i in oper) {
-            let args = stack.splice(stack.length - need[i], need[i]);
-            stack.push(oper[i](...args))
-        } else if (i in vars) {
-            stack.push(variable(i))
+    source.split(' ').filter(s => s.length > 0).forEach(token => {
+        if (token in consts) {
+            stack.push(consts[token])
+        } else if (token in oper) {
+            let args = stack.splice(stack.length - need[token], need[token]);
+            stack.push(oper[token](...args))
+        } else if (token in vars) {
+            stack.push(VARIABLE[token]);
         } else {
-            stack.push(cnst(parseInt(i)));
+            stack.push(cnst(Number(token)));
         }
-    }
+    });
     return stack.pop()
 };
 
 
-console.log(parse('3 y -'))
+console.log(parse('3.3 y -'))
--- objectExpression.js
+++ objectExpression.js
@@ -1,6 +1,158 @@
-const isConst = x => x instanceof Const;
-const isZero = x => isConst(x) && x.evaluate() === 0;
-const isOne = x => isConst(x) && x.evaluate() === 1;
+let Operation = {
+    evaluate : function (...args) {return this.getOperation()(...this.getArgs().map(arg => arg.evaluate(...args)))},
+    diff : function (name) {
+        return this.getDiff()(...this.getArgs(), ...this.getArgs().map(arg => arg.diff(name)));
+    },
+    prefix : function () {
+        let res = "(" + this.getSymbol() + " ";
+        let i = 0;
+        let to = this.getArgs().length - 1;
+        while (i < to) {
+            res += this.getArgs()[i].prefix() + " "; i++;
+        }
+        res += this.getArgs()[to].prefix() + ")";
+        return res;
+    },
+    postfix : function () {
+        let res = "(";
+        let i = 0;
+        let to = this.getArgs().length;
+        while (i < to) {
+            res += this.getArgs()[i].postfix() + " "; i++;
+        }
+        if (to === 0) {
+            res += " ";
+        }
+        res += this.getSymbol() + ")";
+        return res;
+    },
+    simplify : function () {
+        let simpleArgs = this.getArgs().map(arg => arg.simplify());
+        let onlyConst = 1;
+        simpleArgs.forEach(token => {if (!isConst(token)) {onlyConst = 0;}});
+        if (onlyConst) {
+            return new Const(new this.constructor(...simpleArgs).evaluate());
+        }
+        if (this.getSimplifier() !== undefined) {
+            return this.getSimplifier()(...simpleArgs);
+        }
+        return this.constructor(...simpleArgs);
+    }
+};
+function generator(sym, op, diff_impl, simpl_impl) {
+    let ret = function (...arg) {
+        this.getArgs = function () {
+            return arg;
+        };
+        this.getOperation = function () {
+            return op;
+        };
+        this.getSymbol = function () {
+            return sym;
+        };
+        this.getDiff = function () {
+            return diff_impl;
+        };
+        this.getSimplifier = function () {
+            return simpl_impl;
+        };
+    };
+    ret.prototype = Object.create(Operation);
+    ret.prototype.constructor = ret;
+    return ret;
+}
+
+const Negate = generator("negate", a => -a, (a, da) => new Negate(da));
+const Sumexp = generator("sumexp",
+    function (...args) {
+        let ans = 0;
+        if (args.length === 0) {
+            return 0;
+        }
+        for (i of args) {
+            ans += Math.pow(Math.E, i);
+        }
+        return ans;
+    }, (a, da) => new Negate(da));
+const Softmax = generator("softmax",
+    function (...args) {
+        let ans = Math.pow(Math.E, args[0]);
+        let res = 0;
+        for (i of args) {
+            res += Math.pow(Math.E, i);
+        }
+        return ans / res;
+    }, (a, da) => new Negate(da));
+const ArcTan = generator("atan", a => Math.atan(a),(a, da) => new Divide(da, new Add(new Const(1), new Multiply(a, a))));
+const Add = generator("+", (a, b) => a + b, (a, b, da, db) => new Add(da, db), (a, b) => {
+    if (isZero(a)) {
+        return b;
+    }
+    if (isZero(b)) {
+        return a;
+    }
+    return new Add(a, b);
+});
+const Subtract = generator("-", (a, b) => a - b,(a, b, da, db) => new Subtract(da, db),(a, b) => {
+    if (isZero(b)) {
+        return a;
+    }
+    if (isZero(a)) {
+        return new Negate(b);
+    }
+    return new Subtract(a, b);
+});
+const ArcTan2 = generator("atan2",(a, b) => Math.atan2(a, b),(a, b, da, db) => new Divide(new Subtract(new Multiply(da, b), new Multiply(db, a)), new Add(new Multiply(a, a), new Multiply(b, b))),
+    (a, b) => {
+        if (isZero(a) && !isZero(b)) {
+            return new Const(0);
+        }
+        if (isOne(b)) {
+            return new ArcTan(a);
+        }
+        return new ArcTan2(a, b);
+    });
+const Multiply = generator("*", (a, b) => a * b, (a, b, da, db) => new Add(new Multiply(a, db), new Multiply(da, b)),
+    (a, b) => {
+        if (isZero(a) || isZero(b)) {
+            return new Const(0);
+        }
+        if (isOne(a)) {
+            return b;
+        }
+        if (isOne(b)) {
+            return a;
+        }
+        return new Multiply(a, b);
+    });
+const Divide = generator("/", (a, b) => a / b,(a, b, da, db) => new Divide(new Subtract(new Multiply(da, b), new Multiply(db, a)), new Multiply(b, b)),
+    (a, b) => {
+        if (isZero(a)) {
+            return new Const(0);
+        }
+        if (isOne(b)) {
+            return a;
+        }
+        return new Divide(a, b);
+    });
+
+const Simple = {
+    simplify : function() {return this;}
+};
+
+const Const = function(val) {
+    this.val = val;
+    this.getValue = function () {
+        return this.val;
+    }
+};
+Const.prototype = Object.create(Simple);
+Const.prototype.evaluate = function () { return this.getValue(); };
+Const.prototype.toString = Const.prototype.prefix = Const.prototype.postfix = function () { return this.getValue().toString(); };
+Const.prototype.diff = function () { return new Const(0); };
+
+let ONE = new Const(1);
+let ZERO = new Const(0);
 
 const vars = {
     "x" : 0,
@@ -11,145 +163,18 @@ const vars = {
     "w" : 5
 };
 
-const Const = function(x) {
-    this.evaluate = () => x;
-    this.toString = () => x + "";
-    this.diff = () => new Const(0);
-    this.simplify = () => this
-};
-
-const Variable = function(x) {
-    this.evaluate = (...args) => args[vars[x]];
-    this.diff = (val) => new Const(val === x ? 1 : 0);
-    this.toString = () => x + "";
-    this.simplify = () => this
-};
-
-const Op = {
-    evaluate: (operation) => (...args) => (...vars) => {
-        return operation(...args.map(arg => arg.evaluate(...vars)));
-    }
-    , toString : (...args) => {
-        let s = "";
-        for (i of args) {
-            s += i + " ";
-        }
-        return s;
-    }
-    , diff : (arg) => (val) => arg.diff(val)
-    , simplify : (operation, Op) => (...args) => {
-        args = args.map(arg => arg.simplify());
-        let onlyConst = 1;
-        args.forEach(token => {if (!isConst(token)) {onlyConst = 0;}});
-        if (onlyConst) {
-            return new Const(operation(...args.map(arg => arg.evaluate())));
-        } else {
-            return new Op(...args);
-        }
+const Variable = function(name) {
+    this.name = name;
+    this.getName = function () {
+        return this.name;
     }
 };
+Variable.prototype = Object.create(Simple);
+Variable.prototype.evaluate = function (...args) { return args[vars[this.getName()]]; };
+Variable.prototype.toString = Variable.prototype.postfix = Variable.prototype.prefix = function () { return this.getName(); };
+Variable.prototype.diff = function (val) { return val === this.getName() ? ONE : ZERO; };
 
-
-function Negate(x) {
-    this.evaluate = Op.evaluate(a => -a)(x);
-    this.diff = (val) => new Negate(Op.diff(x)(val));
-    this.toString = () => Op.toString(x) + "negate";
-    this.simplify = () => Op.simplify(a => -a, Negate)(x);
-}
-
-function ArcTan(x) {
-    this.evaluate = Op.evaluate(a => Math.atan(a))(x);
-    this.diff = (val) => new Divide(Op.diff(x)(val), new Add(new Const(1), new Multiply(x, x)));
-    this.toString = () => Op.toString(x) + "atan";
-    this.simplify = () => Op.simplify(a => Math.atan(a), ArcTan)(x);
-}
-
-function Add(x, y) {
-    this.evaluate = Op.evaluate((a, b) => a + b)(x, y);
-    this.diff = (val) => new Add(Op.diff(x)(val), Op.diff(y)(val));
-    this.toString = () => Op.toString(x, y) + "+";
-    this.simplify = () => {
-        let simple = Op.simplify((a, b) => a + b, Add)(x, y);
-        let simpleX = x.simplify();
-        let simpleY = y.simplify();
-        if (isZero(simpleX)) {
-            return simpleY;
-        }
-        if (isZero(simpleY)) {
-            return simpleX;
-        }
-        return simple;
-    }
-}
-
-function ArcTan2(x, y) {
-    this.evaluate = Op.evaluate((a, b) => Math.atan2(a, b))(x, y);
-    this.diff = (val) => new Divide(new Subtract(new Multiply(Op.diff(x)(val), y), new Multiply(x, Op.diff(y)(val))),
-        new Add(new Multiply(x, x), new Multiply(y, y)));
-    this.toString = () => Op.toString(x, y) + "atan2";
-    this.simplify = () => Op.simplify((a, b) => Math.atan2(a, b), ArcTan2)(x, y);
-}
-
-function Subtract(x, y) {
-    this.evaluate = Op.evaluate((a, b) => a - b)(x, y);
-    this.diff = (val) => new Subtract(x.diff(val), y.diff(val));
-    this.toString = () => Op.toString(x, y) + "-";
-    this.simplify = () => {
-        let simple = Op.simplify((a, b) => a - b, Subtract)(x, y);
-        let simpleX = x.simplify();
-        let simpleY = y.simplify();
-        if (isZero(simpleX)) {
-            return new Negate(simpleY).simplify();
-        }
-        if (isZero(simpleY)) {
-            return simpleX;
-        }
-        return simple;
-    }
-}
-
-function Multiply(x, y) {
-    this.evaluate = Op.evaluate((a, b) => a * b)(x, y);
-    this.toString = () => Op.toString(x, y) + "*";
-    this.diff = (val) => new Add(new Multiply(x.diff(val), y), new Multiply(x, y.diff(val)));
-    this.simplify = () => {
-        let simple = Op.simplify((a, b) => a * b, Multiply)(x, y);
-        let simpleX = x.simplify();
-        let simpleY = y.simplify();
-        if (isZero(simpleX) || isZero(simpleY)) {
-            return new Const(0);
-        }
-        if (isOne(simpleY)) {
-            return simpleX;
-        }
-        if (isOne(simpleX)) {
-            return simpleY;
-        }
-        return simple;
-    }
-}
-
-
-function Divide(x, y) {
-    this.evaluate = Op.evaluate((a, b) => a / b)(x, y);
-    this.toString = () => Op.toString(x, y) + "/";
-    this.diff = (val) => new Divide(new Subtract(
-            new Multiply(x.diff(val), y), new Multiply(x, y.diff(val))), new Multiply(y, y));
-    this.simplify = () => {
-        let simple = Op.simplify((a, b) => a / b, Divide)(x, y);
-        let simpleX = x.simplify();
-        let simpleY = y.simplify();
-        if (isZero(simpleX)) {
-            return new Const(0);
-        }
-        if (isOne(simpleY)) {
-            return simpleX;
-        }
-        return simple;
-    }
-}
-
-const oper = {
+const OPER = {
     "+" : Add,
     "-" : Subtract,
     "*" : Multiply,
@@ -158,8 +183,7 @@ const oper = {
     "atan" : ArcTan,
     "atan2" : ArcTan2
 };
-
-const need = {
+const NEED = {
     "+" : 2,
     "-" : 2,
     "*" : 2,
@@ -169,21 +193,209 @@ const need = {
     "atan2" : 2
 };
 
-function parse(source) {
-    let stack = [];
-    const tokens = source.split(' ').filter(s => s.length > 0).forEach(token => {
-        if (token in vars) {
-            stack.push(new Variable(token))
-        } else if (token in oper) {
-            let args = stack.splice(stack.length - need[token], need[token]);
-            stack.push(new oper[token](...args))
-        } else {
-            stack.push(new Const(parseInt(token)))
-        }
-    });
-    return stack.pop()
+function ParserException(message, expr, ind) {
+    if (expr === null) {
+        this.message = message;
+    } else {
+        this.message = message + " in index '" + ind + "'\n'" + expr + "'\n";
+        for (var i = 0; i < ind + 1; i++)
+            this.message += " ";
+        this.message += "^\n";
+    }
 }
 
+ParserException.prototype = Object.create(Error.prototype);
+ParserException.prototype.name = "ParserException";
+ParserException.prototype.constructor = ParserException;
 
-let A = parse('x y z * /').diff('y').simplify().evaluate(1, 2, 3);
-console.log(A);
+let ind = 0;
+let stack = [];
+let exp = "";
+
+isDigit = (a) => (a >= "0" && a <= "9") || (ind < exp.length && a === '-' &&
+    exp.charAt(ind + 1) <= '9' && exp.charAt(ind + 1) >= "0");
+isLetter = (a) => (a >= "a" && a <= "z") || (a <= "Z" && a >= "A");
+skipWhiteSpaces = () => {
+    while (ind < exp.length && exp.charAt(ind) === " ") {
+        ind++;
+    }
+};
+
+parseNumber = () => {
+    let ans = "";
+    while (ind < exp.length && isDigit(exp.charAt(ind))) {
+        let cur = exp.charAt(ind);
+        ans += cur;
+        ind++;
+    }
+    return Number(ans);
+};
+
+parseWord = () => {
+    let ans = "";
+    while (ind < exp.length && exp.charAt(ind) !== ")" && exp.charAt(ind) !== " " && exp.charAt(ind) !== '(') {
+        let cur = exp.charAt(ind);
+        ans += cur;
+        ind++;
+    }
+    return ans;
+};
+
+checkChar = () => exp.charAt(ind);
+last = () => stack[stack.length - 1];
+
+function parse() {
+    let args = [];
+    while (stack.length > 0 && OPER[last()] === undefined) {
+
+        if (last() === '(') {
+            throw new ParserException("No Operation", exp, stack.length);
+        }
+        args.push(last());
+        stack.pop();
+    }
+    if (stack.length === 0) {
+        throw new ParserException("Don't have Argument", exp, ind);
+    }
+    let op = stack.pop();
+    if (stack.length > 0 && last() === '(') {
+        stack.pop();
+    } else {
+        throw new ParserException("No open Brace", exp, ind);
+    }
+    args.reverse();
+    if (NEED[op] !== args.length) {
+        throw new ParserException("Too many args", exp, ind);
+    }
+    return new OPER[op](...args);
+}
+
+function parsePost() {
+    let args = [];
+    let op = stack.pop();
+    console.log(op);
+    if (OPER[op] === undefined) {
+        throw new ParserException("No Operation", exp, ind)
+    }
+    while (stack.length > 0 && last() !== '(') {
+        args.push(last());
+        stack.pop();
+    }
+    if (stack.length === 0) {
+        throw new ParserException("Don't have Argument", exp, ind);
+    }
+    console.log(last());
+    if (stack.length > 0 && last() === '(') {
+        stack.pop();
+    } else {
+        throw new ParserException("Don't Have Open Brace", exp, ind);
+    }
+    args.reverse();
+    if (NEED[op] !== args.length) {
+        throw new ParserException("Too many args", exp, ind);
+    }
+    return new OPER[op](...args);
+}
+
+function parsePostfix(source) {
+    ind = 0;
+    stack = [];
+    let balance = 0;
+    exp = source;
+    if (exp === "") {
+        throw new ParserException("Empty input", null, 0);
+    }
+    while (true) {
+        skipWhiteSpaces();
+        if (ind >= exp.length) {
+            break;
+        }
+        let cur = exp.charAt(ind);
+        if (isDigit(cur)) {
+            stack.push(new Const(parseNumber()));
+            continue;
+        }
+        cur = checkChar();
+        if (cur === '(') {
+            stack.push('(');
+            balance++;
+            ind++;
+            continue;
+        } else if (cur === ')') {
+            balance--;
+            if (balance < 0) {
+                throw new ParserException("Not Enough Brackets", exp, ind);
+            }
+            ind++;
+            stack.push(parsePost());
+            continue;
+        }
+        cur = parseWord();
+        if (vars[cur] !== undefined) {
+            stack.push(new Variable(cur));
+        } else if (cur in OPER) {
+            stack.push(cur);
+        } else {
+            throw new ParserException("Argument undefined", exp, ind);
+        }
+    }
+    if (balance > 0) {
+        throw new ParserException("not Enough Brackets", exp, ind);
+    }
+    if (stack.length > 1) {
+        throw new ParserException("Not Enough Operation after all arguments", null, exp.length);
+    }
+    return stack.pop();
+}
+
+function parsePrefix(source) {
+    ind = 0;
+    stack = [];
+    let balance = 0;
+    exp = source;
+    if (exp === "") {
+        throw new ParserException("Empty input", null, 0);
+    }
+    while (true) {
+        skipWhiteSpaces();
+        if (ind >= exp.length) {
+            break;
+        }
+        let cur = exp.charAt(ind);
+        if (isDigit(cur)) {
+            stack.push(new Const(parseNumber()));
+            continue;
+        }
+        cur = checkChar();
+        if (cur === '(') {
+            stack.push('(');
+            balance++;
+            ind++;
+            continue;
+        } else if (cur === ')') {
+            balance--;
+            if (balance < 0) {
+                throw new ParserException("Not Enough Brackets", exp, ind);
+            }
+            ind++;
+            stack.push(parse());
+            continue;
+        }
+        cur = parseWord();
+        if (vars[cur] !== undefined) {
+            stack.push(new Variable(cur));
+        } else if (cur in OPER) {
+            stack.push(cur);
+        } else {
+            throw new ParserException("Argument undefined", exp, ind);
+        }
+    }
+    if (balance > 0) {
+        throw new ParserException("not Enough Brackets", exp, ind);
+    }
+    if (stack.length > 1) {
+        throw new ParserException("Not Enough Operation after all arguments", null, exp.length);
+    }
+    return stack.pop();
+}
+console.log(parsePostfix('(x 2 +)'));
